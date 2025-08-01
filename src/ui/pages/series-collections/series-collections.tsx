755515284ba2a698debad92e43d64a848b608d2e
Ben Archer <ben.archer@hotmail.co.uk>
2025-08-01 20:55:45 +0100

    add all page to series

--- src/ui/pages/series-collections/series-collections.tsx
+++ src/ui/pages/series-collections/series-collections.tsx
@@ -6,9 +6,9 @@ export const SeriesCollectionsPage: FC = () => {
   return (
     <div className="series-collections">
       <CollectionContinueWatching title="Continue Watching" assetType="series" />
-      <CollectionRecentlyAdded title="Recently Added" assetType="series" />
-      <CollectionGenre title="Action" assetType="series" genre="action" />
-      <CollectionGenre title="Comedy" assetType="series" genre="comedy" />
+      <CollectionRecentlyAdded title="Recently Added" assetType="series" showAllCard />
+      <CollectionGenre title="Action" assetType="series" genre="action" showAllCard />
+      <CollectionGenre title="Comedy" assetType="series" genre="comedy" showAllCard />
     </div>
   );
 };
